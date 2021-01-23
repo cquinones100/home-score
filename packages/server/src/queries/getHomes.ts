0542b5817b98c5e57de03da654e310acb849a696
Carlos Quinones <cquinones100@gmail.com>
2021-01-22 22:07:05 -0500

    fix gethomes sql

--- packages/server/src/queries/getHomes.ts
+++ packages/server/src/queries/getHomes.ts
@@ -8,53 +8,54 @@ const getHomes = ({ user_id }: { user_id?: number } = {}) => {
   const bindings = [userIdQuery].filter((binding) => binding !== null);
 
   const query = dbConnection.raw(`
+    with users_categories_homes as (
+      select users.user_id,
+        users.name    as user_name,
+        case
+          when count(categories_homes.*) = 0
+            then
+            '{}'
+          else
+            array_agg(
+              jsonb_build_object(
+                'name', categories.name,
+                'weight', categories.weight,
+                'score', categories_homes.value,
+                'user_id', categories.user_id,
+                'category_id', categories.category_id
+              )
+              order by categories.name
+            )
+          end as categories,
+        homes.home_id,
+        sum(categories_homes.value * weight) / sum(categories.weight * 10) as score
+      from users
+        left outer join categories using (user_id)
+        left outer join categories_homes using (category_id)
+        full outer join homes using (home_id)
+      group by users.user_id, homes.home_id
+    )
+
     select
-      url,
+      categories,
+      homes.url,
       address,
-      home_id,
+      homes.home_id,
+      users_categories_homes.user_name as user_name,
       array_agg(
         jsonb_build_object(
-          'user_name', user_name,
-          'score', score
+            'user_name', user_name,
+            'score', score
         )
       ) as scores,
       avg(score) as score,
       categories,
-      array_agg(home_image_urls_url) as image_urls
-    from (
-      select
-        homes.url,
-        address,
-        homes.home_id,
-        users.name as user_name,
-        sum(value * weight) / sum(weight * 10) as score,
-        case
-        when count(categories_homes.*) = 0
-        then
-          '{}'
-        else
-          array_agg(
-            jsonb_build_object(
-              'name', categories.name,
-              'weight', categories.weight,
-              'score', categories_homes.value,
-              'user_id', categories.user_id,
-              'category_id', categories.category_id
-            )
-            order by categories.name
-          )
-        end as categories,
-        home_image_urls.url as home_image_urls_url
-      from homes
-        left join categories_homes using(home_id)
-        left join categories using(category_id)
-        left join users using(user_id)
-        left join home_image_urls using(home_id)
-      ${userIdQuery === null ? '' : '?'}
-      group by homes.url, address, users.name, homes.home_id, home_image_urls.url
-    ) as inner_query
-    where home_image_urls_url is not null
-    group by url, address, home_id, categories
+      array_agg(home_image_urls.url) as image_urls
+    from homes
+    left join home_image_urls using (home_id)
+    full join users_categories_homes on true
+    ${userIdQuery === null ? '' : '?'}
+    group by categories, homes.url, homes.address, homes.home_id, user_name
   `, bindings)
 
   return (
