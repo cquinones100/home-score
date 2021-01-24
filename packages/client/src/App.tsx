33014859b8ee88e6fb08d2e1b222de1dea3c57b4
Carlos Quinones <cquinones100@gmail.com>
2021-01-24 18:20:10 -0500

    style signup pages

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -16,8 +16,18 @@ function App() {
   return (
     <Router basename='home-score/'>
       <Switch>
-        <Route path='/login' component={Login} />
-        <Route path='/signup' component={Signup} />
+        <div
+          className='container container-fluid pt-5'
+          style={{
+            height: '100vh',
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center'
+          }}
+        >
+          <Route path='/login' component={Login} />
+          <Route path='/signup' component={Signup} />
+        </div>
         <PrivateRoute
           path={'/homes/:id'}
           component={Show as unknown as FC<RouterProps>}
